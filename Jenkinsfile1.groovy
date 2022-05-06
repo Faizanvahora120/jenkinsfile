a121f7d247a11c4ec5d9db76d50a4a1839df0609
Faizan <vahorafaizan152@gmail.com>
2022-05-06 17:55:06 +0530

    changing download step

--- Jenkinsfile1.groovy
+++ Jenkinsfile1.groovy
@@ -6,7 +6,7 @@ pipeline {
             {
                 steps
                 {
-                    bat 'http://github.com/Faizanvahora120/jenkinsfile.git'
+                    bat git branch: 'main', url: 'https://github.com/Faizanvahora120/jenkinsfile.git'
                 }
             }
            stage('Deployment')
